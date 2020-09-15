12b8e7f6f320ee6bc5f33b8574e8000ff4c94334
Adewunmi97 <shonibareadewunmicomfort@gmail.com>
2020-09-15 11:27:07 -0700

    solve bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,3 +18,26 @@ def bubble_sort(arr)
 end
 
 print bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(arr)
+  sorted_arr = arr
+  status = 0
+
+  while status
+    status = false
+    (0...arr.length - 1).each do |i|
+      next unless yield(sorted_arr[i], sorted_arr[i + 1]).positive?
+
+      temp = sorted_arr[i]
+      sorted_arr[i] = sorted_arr[i + 1]
+      sorted_arr[i + 1] = temp
+      status = true
+    end
+  end
+
+  sorted_arr
+end
+
+print bubble_sort_by(%w[hi hello hey aaaaaaa b CCC ddddd]) { |left, right|
+  left.length - right.length
+}
