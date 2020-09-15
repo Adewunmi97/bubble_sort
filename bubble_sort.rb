a0f08ac2c2fa073f821b082e32399eac9b365fb6
khalilhamdii <khaalil.hamdi@gmail.com>
2020-09-15 10:16:30 +0100

    Fixed some linters errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,37 +1,20 @@
-def bubble_sort_1(arr)
+def bubble_sort(arr)
   sorted_arr = arr
+  status = 0
 
-  for i in 0...arr.length - 1
-    for j in 0...arr.length - 1
-
-      next unless sorted_arr[j] > sorted_arr[j + 1]
-
-      temp = sorted_arr[j]
-      sorted_arr[j] = sorted_arr[j + 1]
-      sorted_arr[j + 1] = temp
-      end
-  end
-
-  sorted_arr
-end
-
-def bubble_sort_2(arr)
-  sorted_arr = arr
-  swape = 0
-
-  while swape
-    swape = false
+  while status
+    status = false
     (0...arr.length - 1).each do |i|
       next unless sorted_arr[i] > sorted_arr[i + 1]
 
       temp = sorted_arr[i]
       sorted_arr[i] = sorted_arr[i + 1]
       sorted_arr[i + 1] = temp
-      swape = true
+      status = true
     end
   end
 
   sorted_arr
 end
 
-print bubble_sort_2([4, 3, 78, 2, 0, 2])
+print bubble_sort([4, 3, 78, 2, 0, 2])
